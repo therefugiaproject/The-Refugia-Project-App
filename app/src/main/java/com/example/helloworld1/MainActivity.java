0620bce4f5637296447f4621b1c189a777d3d8df
Lanaparezanin <lanaparezanin@lclark.edu>
2020-09-13 20:48:27 -0700

    Changed layout color

--- app/src/main/java/com/example/helloworld1/MainActivity.java
+++ app/src/main/java/com/example/helloworld1/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.helloworld1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
